5bd2935991579c7e59192bcaffd3404f216d2f62
Quent-in <pages_quentin@hotmail.com>
2015-12-27 13:15:52 +0100

    French language file

---
+++ translations/harbour-expense.fr.ts
@@ -0,0 +1,264 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it_IT">
+<context>
+    <name>AddCurrencyDialog</name>
+    <message>
+        <source>New Currency</source>
+        <translation>Nouvelle devise</translation>
+    </message>
+    <message>
+        <source>Currency</source>
+        <comment>placeholder for currency</comment>
+        <translation>Devise</translation>
+    </message>
+</context>
+<context>
+    <name>CategoryPage</name>
+    <message>
+        <source>Delete Category</source>
+        <translation>Supprimer la catégorie</translation>
+    </message>
+    <message>
+        <source>in %1 this month</source>
+        <comment>subtitle of the amount spent in the CategoryView</comment>
+        <translation>de %1 ce mois-ci</translation>
+    </message>
+    <message>
+        <source>%1 %</source>
+        <translation>%1 %</translation>
+    </message>
+    <message>
+        <source>of the total</source>
+        <comment>subtitle of the percentagebar</comment>
+        <translation>du total</translation>
+    </message>
+    <message>
+        <source>This month:</source>
+        <translation>Ce mois-ci : </translation>
+    </message>
+    <message>
+        <source>amount: %1 %2</source>
+        <comment>1 is amount and 2 is currency</comment>
+        <translation>monotant : %1 %2</translation>
+    </message>
+    <message>
+        <source>%1 %2</source>
+        <comment>1 is amount and 2 is currency</comment>
+        <translation>%1 %2</translation>
+    </message>
+    <message>
+        <source>Add Entry</source>
+        <translation>Ajouter une entrée</translation>
+    </message>
+</context>
+<context>
+    <name>ContactsPage</name>
+    <message>
+        <source>About</source>
+        <translation>À propos</translation>
+    </message>
+    <message>
+        <source>Expense</source>
+        <translation>Dépenses</translation>
+    </message>
+    <message>
+        <source>Version %1</source>
+        <translation>Versione %1</translation>
+    </message>
+    <message>
+        <source>A simple app to manage your money. This software is open source:</source>
+        <translation>Une application simple pour gérer votre argent. Ce logiciel est libre : </translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>This month:</source>
+        <translation>Ce mois-ci : </translation>
+    </message>
+    <message>
+        <source>%1 %2</source>
+        <comment>1 is amount and 2 is currency</comment>
+        <translation>%1 %2</translation>
+    </message>
+</context>
+<context>
+    <name>DeleteCategoryDialog</name>
+    <message>
+        <source>Delete Category</source>
+        <translation>Supprimer une catégorie</translation>
+    </message>
+    <message>
+        <source>!</source>
+        <comment>The exclamation mark in the DeleteCategory View</comment>
+        <translation>!</translation>
+    </message>
+    <message>
+        <source>All your data related with this category (%1) will be lost!!</source>
+        <translation>Toutes les données liées à la catégorid “%1” seront perdues !</translation>
+    </message>
+</context>
+<context>
+    <name>DeleteEntryDialog</name>
+    <message>
+        <source>Delete Item</source>
+        <translation>Supprimer une entrée</translation>
+    </message>
+    <message>
+        <source>amount: %1 %2</source>
+        <comment>1 is amount and 2 is currency</comment>
+        <translation>montant : %1 %2</translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <source>Last Month: %1 %2</source>
+        <comment>1 is amount and 2 is currency</comment>
+        <translation>Le mois dernier : %1 %2</translation>
+    </message>
+    <message>
+        <source>Total: %1 %2</source>
+        <comment>1 is amount and 2 is currency</comment>
+        <translation>Total : %1 %2</translation>
+    </message>
+    <message>
+        <source>Most used category: %1</source>
+        <translation>Catégorie la plus utilisée : %1</translation>
+    </message>
+    <message>
+        <source>History</source>
+        <translation>Historique</translation>
+    </message>
+    <message>
+        <source>Categories</source>
+        <translation>Catégorie</translation>
+    </message>
+    <message>
+        <source>Quick Add</source>
+        <translation>Ajout rapide</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Paramètres</translation>
+    </message>
+    <message>
+        <source>spent this month</source>
+        <comment>subtitle of the amount spent in the MainView</comment>
+        <translation>dépensés ce mois-ci</translation>
+    </message>
+    <message>
+        <source>Expense</source>
+        <comment>This is the App Title</comment>
+        <translation>Dépenses</translation>
+    </message>
+</context>
+<context>
+    <name>MonthSummaryPage</name>
+    <message>
+        <source>%1 %2 in %3</source>
+        <comment>1 is amount, 2 is currency and 3 is the category</comment>
+        <translation>%1 %2 dans %3</translation>
+    </message>
+    <message>
+        <source>Today</source>
+        <translation>Aujourd’hui</translation>
+    </message>
+</context>
+<context>
+    <name>MonthsPage</name>
+    <message>
+        <source>History</source>
+        <translation>Historique</translation>
+    </message>
+    <message>
+        <source>%1 %2</source>
+        <comment>1 is amount and 2 is currency</comment>
+        <translation>%1 %2</translation>
+    </message>
+</context>
+<context>
+    <name>NewCategoryDialog</name>
+    <message>
+        <source>Choose a Name</source>
+        <translation>Entrez un nom</translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <comment>placeholder for category name</comment>
+        <translation>Nom</translation>
+    </message>
+</context>
+<context>
+    <name>NewEntryDialog</name>
+    <message>
+        <source>New Entry</source>
+        <translation>Nouvelle entrée</translation>
+    </message>
+    <message>
+        <source>Amount</source>
+        <comment>placeholder for amount</comment>
+        <translation>Montant</translation>
+    </message>
+    <message>
+        <source>Desc</source>
+        <comment>placeholder for description</comment>
+        <translation>Description</translation>
+    </message>
+    <message>
+        <source>Choose Category</source>
+        <translation>Sélectionnez la catégorie</translation>
+    </message>
+</context>
+<context>
+    <name>ResetDatabaseDialog</name>
+    <message>
+        <source>Reset Database</source>
+        <translation>Réinitialisation de la base de données</translation>
+    </message>
+    <message>
+        <source>!</source>
+        <comment>The exclamation mark in the DeleteCategory View</comment>
+        <translation>!</translation>
+    </message>
+    <message>
+        <source>All your data will be irreversibly lost!!</source>
+        <translation>Toutes vos données seront perdues !</translation>
+    </message>
+</context>
+<context>
+    <name>SecondPage</name>
+    <message>
+        <source>Add Category</source>
+        <translation>Ajouter une catégorie</translation>
+    </message>
+    <message>
+        <source>Categories</source>
+        <translation>Catégorie</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Currency: </source>
+        <translation>Devise : </translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Paramètres</translation>
+    </message>
+    <message>
+        <source>Reset Database</source>
+        <translation>Effacer la base de données</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>À propos</translation>
+    </message>
+    <message>
+        <source>Add New Currency</source>
+        <translation>Ajouter une nouvelle devise</translation>
+    </message>
+</context>
+</TS>
